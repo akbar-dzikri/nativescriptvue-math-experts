1be71ac242eebe8505de5f4e39376a1a7edca367
korban-mantan <mubarokakbar13@gmail.com>
2024-01-01 20:11:55 +0700

    changes app icon and splash screen

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,5 @@
 <template>
     <Page>
-        <ActionBar>
-            <Label text="Home"/>
-        </ActionBar>
-
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
+        <label text="This is the fucking home page"/>
     </Page>
 </template>
-
-<script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
-    }
-  };
-</script>
-
-<style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
-
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
-</style>
---
+++ app/components/Landing.vue
@@ -0,0 +1,39 @@
+<script>
+import Home from './Home.vue'
+
+export default {
+    data() {
+        return {
+            message: 'Hello World!'
+        }
+    },
+    methods: {
+        toHome(){
+            this.$navigateTo(Home)
+        }
+    }
+}
+</script>
+
+<template>
+    <Page>
+        <StackLayout>
+            <Image src='~/../App_Resources/Android/src/main/res/drawable-hdpi/landing.png' stretch="none" />
+            <Button text="Mulai" @tap="toHome"/>
+        </StackLayout>
+
+    </Page>
+</template>
+
+<style lang="scss">
+StackLayout{
+    max-width: 100%;
+    vertical-align: middle;
+    horizontal-align: center;
+    height: auto;
+}
+Button{
+    max-width: 80%;
+    border-radius: 25px;
+}
+</style>
